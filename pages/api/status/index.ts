88ff8e0791a772a14f7bf374fe62086eb3be03b5
Roger Bariles <rogerbariles@gmai.com>
2022-07-22 14:13:18 -0300

    Fix api/status

--- pages/api/status/index.ts
+++ pages/api/status/index.ts
@@ -4,9 +4,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const { method } = req;
 
-    if (method == 'GET') {
-        return res.json({
-            status: 'ok'
-        });
-    }
+    // if (method == 'GET') {
+        
+    // }
+
+    return res.json({
+        status: 'ok'
+    });
 }
