73263f23f97cf9a333f1f9fd4522beb873c7415e
Roger Bariles <rogerbariles@gmai.com>
2022-07-26 08:01:54 -0300

    fix refounds

--- pages/api/refounds/index.ts
+++ pages/api/refounds/index.ts
@@ -24,7 +24,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { method } = req;
     const { body } = req;
 
-    console.log(`req: ${JSON.stringify(req)}`)
+    console.log(`body: ${JSON.stringify(body)}`)
+    console.log(`method: ${JSON.stringify(method)}`)
+    console.log(`url: ${JSON.stringify(req.url)}`)
+
     if (method == 'POST') {
         let requestRefounds: RequestRefounds = body;
         console.log(`requestRefounds: ${JSON.stringify(requestRefounds)}`);
@@ -48,5 +51,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 status: 'ok'
             });
         }
+    }
+
+    if(method == 'OPTIONS') {
+        return res.json('token');
     }
 }
